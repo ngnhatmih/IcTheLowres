efbf2649121469b74bfc65578204eb49c5ebb850
ngnhatmih <ngnhatmih9997@gmail.com>
2024-05-13 08:08:12 +0700

    Added input handler

---
+++ include/input.h
@@ -0,0 +1,30 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <SDL3/SDL.h>
+#include <vector>
+#include "vector2d.h"
+
+enum MouseButtons {
+    LEFT = 0,
+    MIDDLE = 1,
+    RIGHT = 2
+};
+
+class Input {
+public:
+    Input();
+    ~Input();
+
+    void update();
+    void onMouseMove(SDL_Event &event);
+    void onMouseButtonDown(SDL_Event &event);
+    void onMouseButtonUp(SDL_Event &event);
+
+private:
+    Vector2D *mouse_position;
+    std::vector<bool> mouse_buttons;
+
+};
+
+#endif // INPUT_H
---
+++ src/input.cpp
@@ -0,0 +1,83 @@
+#include "input.h"
+#include "game.h"
+
+void Input::update()
+{
+    SDL_Event event;
+    while(SDL_PollEvent(&event))
+    {
+        switch(event.type)
+        {
+            case SDL_EVENT_QUIT:
+                Game::getInstance().clean();
+                break;
+            case SDL_EVENT_MOUSE_MOTION:
+                onMouseMove(event);
+                break;
+            case SDL_EVENT_MOUSE_BUTTON_DOWN:
+                onMouseButtonDown(event);
+                break;
+            case SDL_EVENT_MOUSE_BUTTON_UP:
+                onMouseButtonUp(event);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void Input::onMouseMove(SDL_Event &event)
+{
+    mouse_position->setX(event.motion.x);
+    mouse_position->setY(event.motion.y);
+}
+
+void Input::onMouseButtonDown(SDL_Event &event)
+{
+    switch (event.button.button)
+    {
+        case SDL_BUTTON_LEFT:
+            mouse_buttons[LEFT] = true;
+            break;
+        case SDL_BUTTON_MIDDLE:
+            mouse_buttons[MIDDLE] = true;
+            break;
+        case SDL_BUTTON_RIGHT:
+            mouse_buttons[RIGHT] = true;
+            break;
+        default:
+            break;
+    }
+}
+
+void Input::onMouseButtonUp(SDL_Event &event)
+{
+    switch (event.button.button)
+    {
+        case SDL_BUTTON_LEFT:
+            mouse_buttons[LEFT] = true;
+            break;
+        case SDL_BUTTON_MIDDLE:
+            mouse_buttons[MIDDLE] = true;
+            break;
+        case SDL_BUTTON_RIGHT:
+            mouse_buttons[RIGHT] = true;
+            break;
+        default:
+            break;
+    }
+}
+
+Input::Input()
+{
+    mouse_position = new Vector2D(0, 0);
+    mouse_buttons.push_back(false);
+    mouse_buttons.push_back(false);
+    mouse_buttons.push_back(false);
+}
+
+Input::~Input()
+{
+    delete mouse_position;
+    mouse_buttons.clear();
+}
